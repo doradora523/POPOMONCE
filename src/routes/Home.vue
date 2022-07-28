1a82b0a33b6ae9d46ed76a5f251e1fb9ef7e690e
whilini <whi0306@naver.com>
2022-07-28 18:38:57 +0900

    config: home slide bar size & color

--- src/routes/Home.vue
+++ src/routes/Home.vue
@@ -9,11 +9,10 @@
     :mousewheel="true"
     :modules="modules"
     :speed="600"
-    :scrollbar="true">
+    :scrollbar="true"
+  >
     <swiper-slide>
-      <div
-        :class="`section_1`"
-        class="section">
+      <div :class="`section_1`" class="section">
         <div class="main">
           <div class="main_wrap">
             <div class="main__img">
@@ -29,7 +28,7 @@
                     <br />
                     문화생활
                   </p>
-                </div> 
+                </div>
                 <div class="text__2">
                   <p>
                     다양한 필터를 통해
@@ -37,15 +36,13 @@
                     원하는 조건의 공연을 만나보세요
                   </p>
                 </div>
-              </div>              
+              </div>
             </div>
             <CircleAnimation />
           </div>
         </div>
         <div class="scroll_guide">
-          <div class="mention">
-            Scroll or Slide down
-          </div>
+          <div class="mention">Scroll or Slide down</div>
           <div class="bar__background">
             <div class="bar__slide"></div>
           </div>
@@ -53,9 +50,7 @@
       </div>
     </swiper-slide>
     <swiper-slide>
-      <div
-        :class="`section_2`"
-        class="section">
+      <div :class="`section_2`" class="section">
         <div class="main">
           <div class="main_wrap">
             <div class="main__textBox">
@@ -64,11 +59,11 @@
                   <p>
                     함께
                     <br />
-                    <span>또는</span> 
+                    <span>또는</span>
                     <br />
                     혼자
                   </p>
-                </div> 
+                </div>
                 <div class="text__2">
                   <p>
                     평소 보고 싶었던 공연을
@@ -77,7 +72,7 @@
                   </p>
                 </div>
                 <CircleAnimation />
-              </div>             
+              </div>
             </div>
             <div class="main__img">
               <div class="img"></div>
@@ -87,9 +82,7 @@
       </div>
     </swiper-slide>
     <swiper-slide>
-      <div
-        :class="`section_3`"
-        class="section">
+      <div :class="`section_3`" class="section">
         <div class="main">
           <div class="main_wrap">
             <div class="main__img">
@@ -101,21 +94,21 @@
                   <p>
                     정보와
                     <br />
-                    <span>예매를</span> 
+                    <span>예매를</span>
                     <br />
                     한번에!
                   </p>
-                </div> 
+                </div>
                 <div class="text__2">
                   <p>
                     한 곳에서 편리하게
                     <br />
-                    궁금했던 공연의 정보를 찾고 
+                    궁금했던 공연의 정보를 찾고
                     <br />
                     빠르게 예매하세요
                   </p>
                 </div>
-              </div>             
+              </div>
             </div>
             <CircleAnimation />
           </div>
@@ -123,9 +116,7 @@
       </div>
     </swiper-slide>
     <swiper-slide>
-      <div
-        :class="`section_4`"
-        class="section">
+      <div :class="`section_4`" class="section">
         <div class="main">
           <div class="main_wrap">
             <div class="main__textBox">
@@ -138,7 +129,7 @@
                     <br />
                     TEAM
                   </p>
-                </div> 
+                </div>
                 <div class="text__2">
                   <span>MEMBER</span>
                   <p>
@@ -152,7 +143,7 @@
                     <a href="https://github.com/gyeol2678">정한결</a>
                   </p>
                 </div>
-              </div>            
+              </div>
             </div>
             <div class="main__img">
               <div class="img"></div>
@@ -179,7 +170,7 @@
     components: {
       Swiper,
       SwiperSlide,
-      CircleAnimation
+      CircleAnimation,
     },
     setup() {
       return {
@@ -187,15 +178,13 @@
       };
     },
     data() {
-      return {
-      };
+      return {};
     },
-
   };
 </script>
 
 <style lang="scss">
-  $bar-slide: 50px;
+  $bar-slide: 30px;
   @-webkit-keyframes slide {
     0% {
       bottom: 100px;
@@ -213,12 +202,10 @@
     }
   }
 
-
   .swiper {
     width: 100vw;
     height: 100vh;
   }
-
 
   // .swiper-slide-active {
   //   .section {
@@ -254,7 +241,7 @@
             // right: 0;
             // transform: translate3d(0, 100%, 0);
 
-            .img{
+            .img {
               width: 100%;
               height: 100%;
               background-size: cover;
@@ -264,7 +251,7 @@
             a {
               text-decoration: none;
               color: #fff;
-              &:hover{
+              &:hover {
                 cursor: pointer;
                 text-shadow: 1px 1px 2px #eee;
               }
@@ -296,13 +283,13 @@
                 position: relative;
                 top: 150px;
                 &::before {
-                  content: "";
-                  width:2px;
+                  content: '';
+                  width: 2px;
                   height: 40px;
                   margin: 10px 0 20px 20px;
                   position: absolute;
                   top: -100px;
-                  background-color:rgb(145, 145, 145);
+                  background-color: rgb(145, 145, 145);
                 }
               }
             }
@@ -315,7 +302,7 @@
       .main {
         .main_wrap {
           .main__img {
-            .img{
+            .img {
               background-image: url(https://images.pexels.com/photos/63328/wells-theatre-norfolk-virginian-seats-63328.jpeg?auto=compress&cs=tinysrgb&w=1600);
             }
           }
@@ -340,16 +327,16 @@
         bottom: 0;
         transform: translateX(-50%);
         .mention {
-          font-size: 16px;
+          font-size: 20px;
           color: #fff;
           margin-bottom: 25px;
           font-weight: 700;
         }
         .bar__background {
           width: 5px;
-          height: 50px;
+          height: 70px;
           border-radius: 2.5px 2.5px 0 0;
-          background-color: #c7c7c7;
+          background-color: #c7c7c7c0;
           display: block;
           justify-content: center;
           position: relative;
@@ -359,21 +346,21 @@
             width: 5px;
             height: $bar-slide;
             border-radius: 2.5px;
-            background-color: #fff;
+            background-color: rgba(255, 255, 255, 0.427);
             display: block;
             position: absolute;
             z-index: 3;
-            animation: slide 1.5s infinite linear;
+            animation: slide 1.2s infinite linear;
           }
         }
       }
     }
     .section_2 {
-      background-color: #F8E8C1;
+      background-color: #f8e8c1;
       .main {
         .main_wrap {
           .main__img {
-            .img{
+            .img {
               background-image: url(https://images.pexels.com/photos/6877441/pexels-photo-6877441.jpeg?auto=compress&cs=tinysrgb&w=1600);
             }
           }
@@ -394,7 +381,7 @@
       .main {
         .main_wrap {
           .main__img {
-            .img{
+            .img {
               background-image: url(https://concerthouse.daegu.go.kr/icms/file/getImage.do?atchFileId=FILE_000000000042767uUDZhgVQ&fileSn=0);
             }
           }
@@ -415,8 +402,8 @@
       .main {
         .main_wrap {
           .main__img {
-            .img{
-              background-color: #3D6151;
+            .img {
+              background-color: #3d6151;
             }
           }
           .main__text {
@@ -436,7 +423,7 @@
     }
   }
   .swiper-pagination-vertical.swiper-pagination-bullets,
-  .swiper-vertical>.swiper-pagination-bullets {
+  .swiper-vertical > .swiper-pagination-bullets {
     right: 20px;
   }
   .swiper-pagination {
